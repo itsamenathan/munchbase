cc92d33c9d779093e32367243c657a98a4d14f08
Nathan Warner <nathan@frcv.net>
2026-06-29 09:58:49 -0600

    fixing a bunch of stuff

---
+++ src/components/sw-update-watcher.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+
+export function SwUpdateWatcher() {
+  useEffect(() => {
+    if (!("serviceWorker" in navigator)) return;
+    const handler = (event: MessageEvent) => {
+      if (event.data?.type === "SW_UPDATED") {
+        window.location.reload();
+      }
+    };
+    navigator.serviceWorker.addEventListener("message", handler);
+    return () => navigator.serviceWorker.removeEventListener("message", handler);
+  }, []);
+  return null;
+}
